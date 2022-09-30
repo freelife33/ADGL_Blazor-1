e81d7d150aaee30219e65945a4ea9f8aad5ee4be
freelife33 <freelife_33@hotmail.com>
2022-09-30 16:30:43 +0300

    30-09-2022

---
+++ ADGL_Blazor/Models/KursTranning.cs
@@ -0,0 +1,11 @@
+﻿namespace ADGL_Blazor.Models
+{
+    public class KursTranning
+    {
+        public int Id { get; set; }
+        public string? CourseName { get; set; }
+        public double Price { get; set; }
+        public bool IsActive { get; set; }
+
+    }
+}
